fd63d2f2907742f31f2b9a79948380ce0b11aa51
LiLi <1470096427@qq.com>
2025-12-14 20:56:18 +0800

    Building a REST API with NestJS and Prisma: Input Validation & Transformation

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -3,6 +3,7 @@ import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+
 @Injectable()
 export class ArticlesService {
   constructor(private prisma: PrismaService) {}
@@ -24,10 +25,13 @@ export class ArticlesService {
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
+    return this.prisma.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} article`;
+    return this.prisma.article.delete({ where: { id } });
   }
 }
